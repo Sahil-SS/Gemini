f8893c84d7f41694f3c50e6a49b025b5204e3714
Sahil Sinha <sinhasahil272004@gmail.com>
2024-12-15 17:56:32 +0530

    First commit
    Final commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./components/Sidebar/sidebar";
+import Main from "./components/Main1/Main";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
+
+export default App;
